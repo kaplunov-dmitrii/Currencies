8b8d165299b74d0e9d65140243c4cc6980e351ca
Dmitrii Kaplunov <kaplunovdmitrij97@gmail.com>
2026-06-28 15:30:58 +0300

    Feat^ add BankResponse and Currency data models for JSON parsing

---
+++ Currency.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json.Serialization;
+
+namespace Currencies
+{
+    public class Currency
+    {
+        [JsonPropertyName("CharCode")]
+        public string Code { get; set; }
+
+        [JsonPropertyName("Name")]
+        public string Name { get; set; }
+
+        [JsonPropertyName("Value")]
+        public double Value { get; set; }
+
+    }
+}
